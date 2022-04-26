90e0927d37d1341807042ec25613fa3fed793260
joarierom14 <61167586+joarierom14@users.noreply.github.com>
2022-04-26 20:24:27 -0300

    Create 09-IntroPHP.php

---
+++ 09-IntroPHP.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+
+$txt = "PHP";
+$num = 224455;
+echo "My first $txt script! Jonas $num";
+
+echo "<br>";
+
+echo "I love " . $txt . "!";
+
+echo "<br>";
+
+$x = 5;
+$y = 4;
+echo $x + $y;
+
+?>
+
+</body>
+</html>
