3da88402ef7febbfcf1af3af0dddbf4d26f39200
joarierom14 <61167586+joarierom14@users.noreply.github.com>
2022-05-04 20:28:20 -0300

    04.05

---
+++ 12-php insert estado.php
@@ -0,0 +1,24 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "5si";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+
+$sql = "INSERT INTO estado(nomeEstado)
+VALUES ('Rio de Janeiro')";
+
+if (mysqli_query($conn, $sql)) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+
+mysqli_close($conn);
+?>
